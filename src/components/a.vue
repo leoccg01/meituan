37d7299334dbeb6421c82f5518e2fcf6b1a00a4a
leoccg01 <1210465909@qq.com>
2019-03-08 17:56:31 +0800

    some

--- src/components/a.vue
+++ src/components/a.vue
@@ -1,15 +1,22 @@
 <template>
  <div>
-    what
+    paga a {{$store.state.money.money}}
+    
  </div>
+ <button type="button" name="button" @click="add">增加</button>
+ <button type="button" name="button" @click="reduce">删减</button>
 </template>
 
 <script>
+import {
+  mapActions
+} from 'vuex'
 export default {
   data () {
     return {
     }
-  }
+  },
+  methods: mapActions('money',['add','reduce'])
 }
 
 </script>
